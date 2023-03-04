e1479aeb04dcb5abfee20520a91f307550b9c71d
Emmanuel Yeboah <noelzappy@gmail.com>
2023-03-04 12:19:34 +0000

    feat: refined text-t-speech

--- src/navigators/Application.tsx
+++ src/navigators/Application.tsx
@@ -1,25 +1,84 @@
-import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar, TouchableOpacity, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { Startup, Chat, Post } from '@/screens';
 import { useTheme } from '@/hooks';
 import { useFlipper } from '@react-navigation/devtools';
+import Tts from 'react-native-tts';
+import { Icon, LinearProgress } from '@rneui/base';
+import * as Animatable from 'react-native-animatable';
+
 import { ApplicationStackParamList } from '../../@types/navigation';
 import Main from './Main';
 import { navigationRef } from './utils';
+import { Text } from 'react-native';
 
 const Stack = createStackNavigator<ApplicationStackParamList>();
 
 // @refresh reset
 const ApplicationNavigator = () => {
-  const { Layout, darkMode, NavigationTheme } = useTheme();
+  const { darkMode, NavigationTheme, Layout, Gutters, Colors } = useTheme();
   const { colors } = NavigationTheme;
 
   useFlipper(navigationRef);
 
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  Tts.addEventListener('tts-start', () => {
+    setShowPlayer(true);
+  });
+  Tts.addEventListener('tts-progress', () => {
+    setShowPlayer(true);
+  });
+  Tts.addEventListener('tts-finish', () => {
+    setShowPlayer(false);
+  });
+  Tts.addEventListener('tts-cancel', () => {
+    setShowPlayer(false);
+  });
+
   return (
     <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
+      {showPlayer && (
+        <Animatable.View animation="fadeIn">
+          <View
+            style={[
+              Layout.row,
+              Layout.justifyContentBetween,
+              Layout.alignItemsCenter,
+              Gutters.smallHPadding,
+              {
+                backgroundColor: Colors.grayLight,
+              },
+            ]}
+          >
+            <Icon
+              name="controller-paus"
+              size={30}
+              color={Colors.dark}
+              type="entypo"
+            />
+
+            <Text>Reading a conversation ...</Text>
+            <TouchableOpacity
+              onPress={() => {
+                Tts.stop();
+                setShowPlayer(false);
+              }}
+            >
+              <Icon
+                name="close"
+                size={30}
+                color={Colors.dark}
+                type="antdesign"
+              />
+            </TouchableOpacity>
+          </View>
+
+          <LinearProgress color={Colors.dark} />
+        </Animatable.View>
+      )}
       <NavigationContainer theme={NavigationTheme} ref={navigationRef}>
         <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
         <Stack.Navigator screenOptions={{ headerShown: false }}>
