085a137d781d4de49a0e2065b74634014753d8f0
Emmanuel Yeboah <noelzappy@gmail.com>
2023-03-05 17:38:33 +0000

    feat: updatd android assets

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,9 @@
 export const Config = {
   // API_URL: `http://${LOCAL_IP}:3000/v1`,
   // BASE_URL: `http://${LOCAL_IP}:3000`,
-  GC_API_KEY: 'GCP_API_KEY',
+
+  GC_API_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+
   GC_API_URL: 'https://vision.googleapis.com/v1/images:annotate?key=',
 
   API_URL: 'https://test.tipme-app.com/v1',
